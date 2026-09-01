16fec31460ccfeef5c6de458e3e271b6a0b14194
Priyankasapp <priyanka.s@applocum.org>
2026-09-01 14:43:06 +0530

    initial commit..

--- app/all-conditions/page.tsx
+++ app/all-conditions/page.tsx
@@ -1,6 +1,11 @@
+'use client';
+import styles from "./AllConditions.module.css"
+
 const AllCondition = () =>{
     return(
-        <div>All Condition</div>
+        <div className={styles["all-conditions-container"]}>
+            <h1>Discover Conditions and Find the Right Care </h1>
+        </div>
     )
 }
 export default AllCondition;
--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,12 +1,33 @@
 import ProductCard from "@/components/Shop/ProductCard/ProductCard";
 import ProductSidebar from "@/components/Shop/ProductSidebar/ProductSidebar";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 
 const Shop = () => {
     return(
         <div>
             <h1 style={{padding:'0% 3% '}}>Shop</h1>
             <ProductSidebar/>
+           <div>
             <ProductCard/>
+            <div>
+                <div style={{
+                    color:"black",
+                    float:"left",
+                    padding:"8px 16px",
+                    textDecoration:"none",
+                    transition:"backgroundColor .3s"
+                }}>
+                    <a><ArrowLeft/> Previous</a>
+                    <a>1</a>
+                    <a>2</a>
+                    <a>3</a>
+                    <a>...</a>
+                    <a>10</a>
+                    <a>16</a>
+                    <a><ArrowRight/> Next</a>
+                </div>
+            </div>
+           </div>
         </div>
     )
 }
