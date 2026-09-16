289f26e101a5083e348e47e6bf886ca5c92356d1
Priyankasapp <priyanka.s@applocum.org>
2026-09-16 11:10:55 +0530

    update hero module..!

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -1,3 +1,4 @@
+import Button from '@/components/Button/Button';
 import styles from './Hero.module.css';
 
 
@@ -5,9 +6,12 @@ const Hero = () => {
   return (
     <div className={styles['hero']}>
       <div className={styles['hero-container']}>
-        <h1 className={styles['hero-header']}>Manage Your Health with Ease.</h1>
+      <div className={styles['hero-text-wrapper']}>
+           <h1 className={styles['hero-header']}>Manage Your <span>Health</span>  with Ease.</h1>
         <p className={styles['hero-text']}>From common ailments to daily medications, our pharmacists provide professional guidance and prescriptions delivered safely to you.</p>
-        <button>Book Appointment Now</button>
+      </div>
+      
+        <Button showArrow>Book Appointment Now</Button>
       </div>
     </div>
   ) 
