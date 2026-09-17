3e6d62bd19dc3af75f6c32881643314f11d3faff
Priyankasapp <priyanka.s@applocum.org>
2026-09-17 13:12:20 +0530

    make treatment card responsive..

--- components/Home/TreatmentCard/TreatmentCard.tsx
+++ components/Home/TreatmentCard/TreatmentCard.tsx
@@ -3,38 +3,45 @@ import styles from "./TreatmentCard.module.css";
 import Image from "next/image";
 import { ChevronRight } from "lucide-react";
 import { TreatmentDataProps } from "@/types/type";
+import { Fleur_De_Leah } from "next/font/google";
 
-const TreatmentCard = ({id, title, img, condition}:TreatmentDataProps) => {
+const TreatmentCard = ({ id, title, img, condition }: TreatmentDataProps) => {
   return (
-    <div className={styles['treatment-card']} id={String(id)}>
-        <div className={styles['treatment-card-container']}></div>
-            <div className={styles['treatment-card-img-wrapper']} >
-            <Image
-            src = {img}
-            alt ={title}
-            className={styles['treatment-card-img']}/>
+    <div className={styles["treatment-card"]} id={String(id)}>
+      <div className={styles["treatment-card-img-wrapper"]}>
+        <Image
+          src={img}
+          alt={title}
+          fill
+          sizes="(max-width: 400px) 100vw, 400px"
+          priority={id <= 3}
+          className={styles["treatment-card-img"]}
+        />
+      </div>
+
+      <div className={styles["treatment-card-info"]}>
+        <h3>{title}</h3>
+        <div className={styles["treatment-card-info-con"]}>
+          {condition.map((con, index) => (
+            <div key={`${id}-${index}`} className={styles["treatment-desc-wrapper"]}>
+              
+              <span >{con}</span>
+                <ChevronRight  className={styles['treatment-arrow-icon']}/>
+              
+              
+            </div>
+
+          ))}
+          <div style={{
+            // display:"flex",
+            // gap:"10px"
+          }}>
+          
+          </div>
         </div>
+      </div>
+    </div>
+  );
+};
 
-        <div className={styles['treatment-card-info']}>
-
-                <h3 >{title}</h3>
-                <div className={styles['treatment-card-info-con']}>
-
-                    {condition.map((con)=>(
-                         <div className={styles['treatment-desc-wrapper']}>
-                        <span>{con}</span>
-                        <ChevronRight/>
-                    </div>
-                    ))}
-                   
-
-                    
-                </div>
-            
-        </div>
-
-           </div>
-  )
-}
-
-export default TreatmentCard
+export default TreatmentCard;
--- components/Home/TreatmentSection/TreatmentSection.tsx
+++ components/Home/TreatmentSection/TreatmentSection.tsx
@@ -1,90 +1,96 @@
 /* eslint-disable react/jsx-key */
 /* eslint-disable react-hooks/set-state-in-effect */
-import  { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 import styles from "./TreatmentSection.module.css";
-import { treatmentData } from '@/lib/data';
-import { ChevronLeft, ChevronRight } from 'lucide-react';
-import TreatmentCard from '../TreatmentCard/TreatmentCard';
+import { treatmentData } from "@/lib/data";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import TreatmentCard from "../TreatmentCard/TreatmentCard";
 
 const getTreatmentCardsPerPage = () => {
-    if(typeof window === "undefined") return 3;
-    if (window.innerWidth <= 600) return 1;
-    if (window.innerWidth <= 900) return 2;
+  if (typeof window === "undefined") return 3;
+  if (window.innerWidth <= 550) return 1;
+  if (window.innerWidth <= 700) return 2;
+  if(window.innerWidth <= 1000) return 3;
 
-    return 3;
+  return 3;
 };
 
 const TreatmentSection = () => {
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const [treatmentCardPerPage, setTreatmentCardPerPage] = useState(3);
-    const [isMounted, setMounted] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [treatmentCardPerPage, setTreatmentCardPerPage] = useState(3);
+  const [isMounted, setMounted] = useState(false);
 
-    useEffect(()=>{
-        setMounted(true);
-        setTreatmentCardPerPage(getTreatmentCardsPerPage);
+  useEffect(() => {
+    setMounted(true);
+    setTreatmentCardPerPage(getTreatmentCardsPerPage);
 
-        const handleResize = () => setTreatmentCardPerPage(getTreatmentCardsPerPage());
-        window.addEventListener("resize", handleResize);
-        return () => window.removeEventListener("resize", handleResize);
+    const handleResize = () =>
+      setTreatmentCardPerPage(getTreatmentCardsPerPage());
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
-    }, []);
+  const visibleTreatmentCards = treatmentData.slice(
+    currentIndex,
+    currentIndex + treatmentCardPerPage,
+  );
+  const handleNext = () => {
+    if (currentIndex + treatmentCardPerPage < treatmentData.length) {
+      setCurrentIndex((prev) => prev + treatmentCardPerPage);
+    }
+  };
 
-    const visibleTreatmentCards = treatmentData.slice(
-        currentIndex,
-        currentIndex + treatmentCardPerPage,
-    );
-    const handleNext = () => {
-        if(currentIndex + treatmentCardPerPage < treatmentData.length){
-            setCurrentIndex((prev) => prev + treatmentCardPerPage);
-        }
-    };
+  const handlePrevious = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex((prev) => prev - treatmentCardPerPage);
+    }
+  };
 
-    const handlePrevious = () => {
-        if(currentIndex > 0){
-            setCurrentIndex((prev)=> prev - treatmentCardPerPage);
-        }
-    };
-
-    
   return (
-    <section className={styles['treatment-section']}>
-        <div className={styles['treatment-header']}>
-            <h2 className={styles['treatment-heading']}>
-                From Better Sleep to Stronger Immunity, Discover <span>Lifestyle Treatments</span> That Improve Your <span>Daily</span> Wellness
-            </h2>
-            <div className={styles['treatment-slider']}>
-                <button
-                type='button'
-                onClick={handlePrevious}
-                disabled={currentIndex === 0}
-                aria-label='Previous Treatment'>
-                    <ChevronLeft/>
-                </button>
-                <button 
-                type='button'
-                onClick={handleNext}
-                disabled={currentIndex + treatmentCardPerPage >=treatmentData.length }
-                aria-label='Next Treatment'>
-                    <ChevronRight/>
-                </button>
+    <section className={styles["treatment-section"]}>
+      <div className={styles["treatment-header"]}>
+        <h2 className={styles["treatment-heading"]}>
+          From Better Sleep to Stronger Immunity, Discover{" "}
+          <span>Lifestyle Treatments</span> That Improve Your <span>Daily</span>{" "}
+          Wellness
+        </h2>
+        <div className={styles["treatment-slider"]}>
+          <button
+            type="button"
+            onClick={handlePrevious}
+            disabled={currentIndex === 0}
+            aria-label="Previous Treatment"
+          >
+            <ChevronLeft />
+          </button>
+          <button
+            type="button"
+            onClick={handleNext}
+            disabled={
+              currentIndex + treatmentCardPerPage >= treatmentData.length
+            }
+            aria-label="Next Treatment"
+          >
+            <ChevronRight />
+          </button>
+        </div>
+      </div>
+      <div className={styles["treatment-card-container"]}>
+        {visibleTreatmentCards.map((treatment) => {
+          return (
+            <div className={styles["treatment-card-item"]}>
+              <TreatmentCard
+                id={treatment.id}
+                img={treatment.img}
+                title={treatment.title}
+                condition={treatment.condition}
+              />
             </div>
-
-        </div>
-        <div className={styles['treatment-card-container']}>
-            {visibleTreatmentCards.map((treatment)=>{
-                return(
-                    <div className={styles['treatment-card-item']}>
-                        <TreatmentCard
-                        id={treatment.id}
-                        img={treatment.img}
-                        title={treatment.title}
-                        condition={treatment.condition}/>
-                    </div>
-                )
-            })}
-        </div>
+          );
+        })}
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default TreatmentSection
+export default TreatmentSection;
