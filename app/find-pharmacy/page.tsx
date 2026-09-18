26e51144b451932f78e3cae2ded14b2d6c3a961d
Priyankasapp <priyanka.s@applocum.org>
2026-09-18 13:57:35 +0530

    add map on find harmacy page..

--- app/find-pharmacy/page.tsx
+++ app/find-pharmacy/page.tsx
@@ -1,49 +1,36 @@
-import FindPharmacyCard from "@/components/Find-Pharmacy/FindPharmacyCard/FindPharmacyCard";
-import FindPharmacyMap from "@/components/Find-Pharmacy/FindPharmacyMap/FindPharmacyMap";
-import SearchBar from "@/components/Find-Pharmacy/SearchBar/SearchBar";
+import FindPharmacyCard from '@/components/Find-Pharmacy/FindPharmacyCard/FindPharmacyCard';
+import FindPharmacyMap from '@/components/Find-Pharmacy/FindPharmacyMap/FindPharmacyMap';
+import styles from "./FindPharmacy.module.css";
+import { Search } from 'lucide-react';
 
 const FindPharmacy = () => {
-    return (
-        <div style={{ padding: "0 4%", marginBottom: "8%" }}>
-            
-            {/* Title Section */}
-            <h1 style={{ fontSize: "2rem", fontWeight: "bold", margin: "24px 0" }}>
-                Find Your Nearest Pharmacy
-            </h1>
+  return (
+    <div className={styles['find-pharmacy']}>
+      <h1 className={styles['find-pharmacy-title']}>Find your Nearest Pharmacy</h1>
 
-            {/* Search Section */}
-            <div style={{ marginBottom: "24px" }}>
-                <SearchBar />
-            </div>
-
-            {/* Main Content Split Section */}
-            <div style={{ 
-                display: "flex", 
-                gap: "32px", 
-                alignItems: "flex-start" 
-            }}>
-                
-                {/* Map Section (Left side - expands to fill space) */}
-                <div style={{ flex: 1, minHeight: "500px" }}>
-                    <FindPharmacyMap />
-                </div>
-
-                {/* Sidebar Section (Right side - fixed width matching design) */}
-                <div style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    gap: "24px",
-                    width: "410px", // Fixed width to prevent card elements from breaking
-                    flexShrink: 0
-                }}>
-                    <FindPharmacyCard />
-                    <FindPharmacyCard />
-                    <FindPharmacyCard />
-                </div>
-
-            </div>
+      <div className={styles['find-pharmacy-searchbar-container']}>
+        <span>Search</span>
+        <div className={styles['find-pharmacy-searchbar-wrapper']}>
+          <input placeholder='Dr Stone pharmacy, 123 High Street'/>
+          <Search className={styles['search-icon']} />
         </div>
-    );
+      </div>
+
+      <div className={styles['find-pharmacy-container']}>
+        {/* Map Bounding Container */}
+        <div className={styles['find-pharmacy-map-wrapper']}>
+          <FindPharmacyMap />
+        </div>
+        
+        {/* Sidebar Cards Panel */}
+        <div className={styles['find-pharmacy-card-list']}>
+          <FindPharmacyCard />
+          <FindPharmacyCard />
+          <FindPharmacyCard />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FindPharmacy;
--- components/Find-Pharmacy/FindPharmacyMap/FindPharmacyMap.tsx
+++ components/Find-Pharmacy/FindPharmacyMap/FindPharmacyMap.tsx
@@ -1,41 +1,48 @@
-
-import React from 'react'
+import React from 'react';
 import styles from "./FindPharmacyMap.module.css";
 import Image from 'next/image';
 import { locationIcon, mapImg } from '@/data/assets';
+
 const FindPharmacyMap = () => { 
   const locations = [
-    {id: 1, top:"33%", left:"21%"},
-    {id: 2, top:"30%", left:"84%"},
-    {id: 3, top:"70%", Left:"72%"},
+    { id: 1, top: "33%", left: "21%" },
+    { id: 2, top: "22%", left: "51%" }, 
+    { id: 3, top: "58%", left: "44%" }, 
   ];
+
   return (
     <div className={styles['pharmacy-map']}>
       <div className={styles['pharmacy-map-con']}>
+        
+        
         <div className={styles['pharmacy-map-img-wrapper']}>
           <Image
-        src={mapImg}
-        fill
-        alt='pharmacy map'
-        className={styles['pharmacy-map-img']}/>
+            src={mapImg}
+            fill
+            alt='pharmacy map'
+            className={styles['pharmacy-map-img']}
+            priority />
         </div>
 
-        {locations.map((loc)=>(
+       
+        {locations.map((loc) => (
           <div
-          key={loc.id}
-          className={styles['pharmacy-location-pin-wraper']}
-          style={{top:loc.top, left:loc.left}}>
-              <Image src={locationIcon}
+            key={loc.id}
+            className={styles['pharmacy-location-pin-wrapper']}
+            style={{ top: loc.top, left: loc.left }}
+          >
+            <Image 
+              src={locationIcon}
               alt='pharmacy location'
-              
-              width={32}
-              height={32}
-              className={styles['pharmacy-location-img']}/>
+              width={42} 
+              height={42}
+              className={styles['pharmacy-location-img']}
+            />
           </div>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FindPharmacyMap
+export default FindPharmacyMap;
