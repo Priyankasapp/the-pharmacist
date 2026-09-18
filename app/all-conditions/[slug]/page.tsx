564f35d67640ba2ade1c417d20ac21d0462ce70d
Priyankasapp <priyanka.s@applocum.org>
2026-09-18 21:50:53 +0530

    initial commit..!

--- app/all-conditions/[slug]/page.tsx
+++ app/all-conditions/[slug]/page.tsx
@@ -1,12 +1,7 @@
 /* eslint-disable react/jsx-key */
-/* eslint-disable react/jsx-no-undef */
-
 "use client";
-import { productCardData, stepInfo } from "@/lib/data";
-import { useState } from "react";
+import {  stepInfo } from "@/lib/data";
 import styles from "./ConditionSlug.module.css";
-import ProductCard from "@/components/Shop/ProductCard/ProductCard";
-import { ChevronLeft, ChevronRight } from "lucide-react";
 import FAQ from "@/components/Contact/FAQ/FAQ";
 import Symptoms from "@/components/all-conditions/Symptoms/Symptoms";
 import StepSectionCard from "@/components/all-conditions/StepSectionCard/StepSectionCard";
@@ -14,30 +9,19 @@ import NHSSupportCard from "@/components/NHSSupportCard/NHSSupportCard";
 import Image from "next/image";
 import { NHS } from "@/data/assets";
 import ProductSection from "@/components/ProductSection/ProductSection";
-const ConditionSlug = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const productPerPage = 3;
-  const visibleProducts = productCardData.slice(
-    currentIndex,
-    currentIndex + productPerPage,
-  );
-  const handleNext = () => {
-    if (currentIndex + productPerPage < productCardData.length) {
-      setCurrentIndex((prev) => prev + productPerPage);
-    }
-  };
-  const handlePrevious = () => {
-    if (currentIndex > 0) {
-      setCurrentIndex((prev) => prev - productPerPage);
-    }
-  };
 
+const ConditionSlug = () => {
   return (
     <div className={styles["condition-page"]}>
       <div className={styles["condition-header"]}>
-        {" "}
         <h2>Sore Throat </h2>
-        <Image src={NHS} alt="NHS" className={styles["header-img"]} />
+        <div className={styles['header-img-wrapper']}>
+        <Image 
+        src={NHS} 
+        alt="NHS" 
+        fill
+        priority />
+        </div>
       </div>
 
       {/* hero section  */}
@@ -46,24 +30,28 @@ const ConditionSlug = () => {
           A sore throat is irritation or pain in the throat, often caused by
           infection or dryness.
         </h1>
-        <NHSSupportCard />
+        <div className={styles['nhs-card-wrapper']}>
+          <NHSSupportCard />
+        </div>
+        
       </div>
       <div className={styles["condition-step-cards"]}>
         {stepInfo.map((step) => (
-          <StepSectionCard id={step.id} desc={step.desc} name={step.name} />
+          <div className={styles["condition-step-card-item"]}>
+            <StepSectionCard id={step.id} desc={step.desc} name={step.name} />
+          </div>
+          
         ))}
       </div>
 
-      {/**/}
-      {/* <StepSectionCard
-      /> */}
+  
       <Symptoms />
 
       {/* product section  */}
       <div className={styles["condition-product-card"]}>
         <ProductSection headingText="Sore Throat Relif Products" />
       </div>
-      <div>
+      <div className={styles["faq-section-wrapper"]}>
         <FAQ />
       </div>
     </div>
