e5d8be7165ed4f4f7103a894d5392746de9e5434
Priyankasapp <priyanka.s@applocum.org>
2026-09-24 15:03:29 +0530

    edit treatment card..!

--- components/Lifestyle-treatmemts/TreatmentsCard/TreatmentCard.tsx
+++ components/Lifestyle-treatmemts/TreatmentsCard/TreatmentCard.tsx
@@ -1,26 +1,31 @@
+
 import styles from "./TreatmentCard.module.css";
 import Image from "next/image";
 import { Treatments } from "@/lib/types";
 import Button from "@/components/Button/Button";
-const TreatmentCard = ({ name, desc, imgSrc}:Treatments) => {
+
+const TreatmentCard = ({ name, desc, imgSrc }: Treatments) => {
   return (
-    <div className={styles["treatement-card"]}>
+    <div className={styles["treatment-card"]}>
       <div className={styles["treatment-card-header"]}>
-        <h3>{name}</h3>
-        <p className={styles["treatment-crard-description"]}>
-          {desc}
-        </p>
+        <h3 className={styles["treatment-card-title"]}>{name}</h3>
+        <p className={styles["treatment-card-description"]}>{desc}</p>
       </div>
 
-      <div className={styles['treatment-card-image-wrapper']}>
+      <div className={styles["treatment-card-image-wrapper"]}>
         <Image
-        src={imgSrc}
-        alt={name}
-        className={styles['treatment-card-Image']}/>
+          src={imgSrc}
+          alt={name}
+          fill
+          sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 423px"
+          className={styles["treatment-card-image"]}
+        />
       </div>
 
-      <div className={styles['treatment-card-button-wrapper']}>
-        <Button className={styles['treatment-card-wrapper-button']} showArrow>View Treatments</Button>
+      <div className={styles["treatment-card-button-wrapper"]}>
+        <Button className={styles["treatment-card-wrapper-button"]} showArrow>
+          View Treatments
+        </Button>
       </div>
     </div>
   );
