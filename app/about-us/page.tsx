bf82647e46bef9c1324b4760e1de4af692030998
Priyankasapp <priyanka.s@applocum.org>
2026-09-25 12:32:10 +0530

    edit about hero page ..

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,42 +1,20 @@
-import Hero from "@/components/about/Hero/Hero";
-import Patients from "@/components/about/Patients/Patients";
-import Specialist from "@/components/about/Specialist/Specialist";
-import Timeline from "@/components/about/Timeline/Timeline";
-import WhatWeOffer from "@/components/about/WhatWeOffer/WhatWeOffer";
-import WhyChosePharmalist from "@/components/WhyChosePharmalist/WhyChosePharmalist";
+import styles from "./AboutUs.module.css";
+import Hero from '@/app/about-us/components/Hero/Hero';
 
 const page = () => {
   return (
-    <div>
+    <main className={styles['about-page']}>
       <div className="container">
-         <h1
-        style={{
-          margin: "0% 4%",
-          fontWeight: "600",
-          fontSize: "40px",
-        }}
-      >
-        About Us
-      </h1>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Hero />
-        <WhatWeOffer />
-        <Timeline />
-        <WhyChosePharmalist />
-        <Specialist />
-        <Patients />
-      </div>
-      </div>
-     
-    </div>
-  );
-};
+        <h1 className={styles['about-title']}>About Us</h1>
 
-export default page;
+
+        <div className={styles['about-content']}>
+          <Hero/>
+          
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default page
